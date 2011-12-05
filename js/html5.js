5de9fc079c28871e4fae786e7c9c67c103287e67
Javier Gamarra <nhpatt@gmail.com>
2011-12-05 16:18:34 +0100

    Refactoring! deleting slides, validating...

--- js/html5.js
+++ js/html5.js
@@ -1,20 +1,27 @@
-$(document).ready(
-		function() {
-			$("#save_local_storage").click(
-					function() {
-						window.localStorage.setItem('value',
-								$("#local_storage").val());
-						window.localStorage.setItem('timestamp', (new Date()));
-					});
-			$("#local_storage").val(window.localStorage.getItem('value'));
-			$("#date_local_storage").text(
-					(new Date(window.localStorage.getItem('timestamp')))
-							.toUTCString());
+$(document)
+		.ready(
+				function() {
+					$("#save_local_storage").click(
+							function() {
+								window.localStorage.setItem('value', $(
+										"#local_storage").val());
+								window.localStorage.setItem('timestamp',
+										(new Date()));
+							});
+					$("#local_storage").val(
+							window.localStorage.getItem('value'));
+					$("#date_local_storage")
+							.text(
+									(new Date(window.localStorage
+											.getItem('timestamp')))
+											.toUTCString());
 
-			var db = window.openDatabase("DBName", "1.0", "description",
-					2 * 1024 * 1024);
-			db.transaction(function (tx) {
-			    tx.executeSql("CREATE TABLE tweets (id unique,screen_name, date integer, text)");
-			  });
-						
-		});
+					var db = window.openDatabase("DBName", "1.0",
+							"description", 2 * 1024 * 1024);
+					db
+							.transaction(function(tx) {
+								tx
+										.executeSql("CREATE TABLE tweets (id unique,screen_name, date integer, text)");
+							});
+
+				});
